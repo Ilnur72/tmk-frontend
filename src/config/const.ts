d6cec6ff0c82d25d23931e8f4a89c86143870377
Ilnur72 <ilnur@MacBook-Pro-Ilnur.local>
2025-09-16 18:36:37 +0500

    Update

---
+++ src/config/const.ts
@@ -0,0 +1,3 @@
+export const API_URL = "http://tmk.bgs.uz/api";
+// export const API_URL = process.env.REACT_APP_API_URL || "http://172.25.24.241:8085";
+export const EMPLOYEE_API_URL = process.env.REACT_APP_API_BASE_URL || "http://84.54.118.39:8444/1c";
